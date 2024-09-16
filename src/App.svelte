1b4341a8ff549de448988438fe44b2701f8aa1c2
JonyUTDT <jjeifetz@mail.utdt.edu>
2024-09-16 14:11:53 -0300

    Entregable-15-9-Con-Iframe

--- src/App.svelte
+++ src/App.svelte
@@ -42,6 +42,11 @@
   }
 </script>
 
+<svelte:head>
+  <iframe src='https://flo.uri.sh/story/2592577/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/story/2592577/?utm_source=embed&utm_campaign=story/2592577' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>
+  <iframe src='https://flo.uri.sh/story/2592457/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/story/2592457/?utm_source=embed&utm_campaign=story/2592457' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>
+</svelte:head>
+
 <!-- HTML -->
 <main>
   <br class="container">
@@ -108,8 +113,8 @@
     <br>
     <br>
     <div class = "GraficosComplementarios">
-      <div class="flourish-embed" data-src="story/2592457"><script src="https://public.flourish.studio/resources/embed.js"></script><noscript><img src="https://public.flourish.studio/story/2592457/thumbnail" width="100%" alt="visualization" /></noscript></div>
-      <div class="flourish-embed" data-src="story/2592577"><script src="https://public.flourish.studio/resources/embed.js"></script><noscript><img src="https://public.flourish.studio/story/2592577/thumbnail" width="100%" alt="visualization" /></noscript></div>
+      <iframe src='https://flo.uri.sh/story/2592457/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/story/2592457/?utm_source=embed&utm_campaign=story/2592457' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>
+      <iframe src='https://flo.uri.sh/story/2592577/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/story/2592577/?utm_source=embed&utm_campaign=story/2592577' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>
     </div>
     <br>
     <br>
